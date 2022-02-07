c6286279e61b27751760364a6c3041d267610e05
Chandelier Ephrata <chandephrata@gmail.com>
2022-02-07 11:32:54 -0700

    commit tree 8460786446df82e4eb1012e1e128a5e5ed53ad97

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string){
+    return string.toUpperCase()
+}
+function whisper(string){
+    return string.toLowerCase()
+}
+const logShout="string"
+    console.log(string.toUpperCase());
+
+
+
+
+
+  
+
